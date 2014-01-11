1bf724fb69335badff3423f6c6b414b3d5e09dbd
Alex Schroeder <alex@gnu.org>
2014-01-11 13:50:04 +0100

    Fix GitHub URL.

--- Character Generator/halberdsnhelmets.pl
+++ Character Generator/halberdsnhelmets.pl
@@ -82,7 +82,7 @@ sub footer {
 	      $q->a({-href=>$url . "/$lang"}, 'Character Sheet Generator'),
 	      $q->a({-href=>$url . "/help/$lang"}, T('Help')),
 	      $q->a({-href=>$url . "/source"}, T('Source')),
-	      $q->a({-href=>"https://github.com/kensanata/halberdsnhelmets/Character%20Generator/"}, T("GitHub")),
+	      $q->a({-href=>"https://github.com/kensanata/halberdsnhelmets/tree/master/Character%20Generator"}, T("GitHub")),
 	      ($lang eq "en"
 	       ? $q->a({-href=>$url . "/de"}, T('German'))
 	       : $q->a({-href=>$url . "/en"}, T('English'))));
