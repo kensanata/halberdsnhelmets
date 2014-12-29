de6dab0355e4e9b30c484ebceb2193461e506d52
Alex Schroeder <alex@gnu.org>
2014-12-29 16:15:41 +0100

    Make merged file

--- Hellebarden und Helme/project.el
+++ Hellebarden und Helme/project.el
@@ -1,3 +1,5 @@
+;; do need the latest org-mode for @<br/> to work?
+;; (add-to-list 'load-path "~/src/org-mode")
 (let ((dir (file-name-directory (buffer-file-name))))
   (setq org-publish-project-alist
 	`(("Hellebarden und Helme"
